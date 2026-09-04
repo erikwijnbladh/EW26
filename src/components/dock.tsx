8718363a2e2d93f6a217413ccfaf62a8e37b9190
ErikWijnbladh <wijnbladherik@gmail.com>
2026-09-04 15:03:18 +0200

    Remove mobile dock scrim

--- src/components/dock.tsx
+++ src/components/dock.tsx
@@ -118,8 +118,6 @@ export function Dock() {
         )}
       </AnimatePresence>
 
-      <div className="dock-scrim pointer-events-none fixed inset-x-0 bottom-0 z-40 h-32" aria-hidden />
-
       <div className="dock-frame pointer-events-none fixed inset-x-0 bottom-0 z-50 flex justify-center">
         <div className="pointer-events-none">
           <ExpandableTabs
